c2e61297430be07101c955b4e40cba5ba501ae4a
Epii <frank.g.taylor97@gmail.com>
2026-08-21 15:26:17 +0200

    feat(story): export story repository contract

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -9,3 +9,4 @@ export * from "./ports/sequence";
 export * from "./ports/scene";
 export * from "./ports/timelineRepository";
 export * from "./ports/placesRepository";
+export * from "./ports/storyRepository";
